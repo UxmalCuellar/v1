6dc36c6d61929fbfbce617b86554685325efe4c8
Uxmal Cuellar <uxmal.c@gmail.com>
2019-06-08 13:56:56 +1000

    added dropdown feature to nav

--- static/script.js
+++ static/script.js
@@ -1,4 +1,20 @@
-// toggle menu icon into an x 
+// toggles menu icon into an x and show/hides menu items
 function toggleMenuIcon(x) {
    x.classList.toggle("change");
+   document.getElementById("menu").classList.toggle("show");
+}
+
+// close dropdown menu if user clicks outside if it
+window.onclick = function(event) {
+   if (!event.target.matches('.toc-holder')) {
+      var dropdowns = document.getElementByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+	 var openDropDown = dropdowns[i];
+	 if (openDropDown.classList.contains('show')) {
+	    alert("hey");
+	    openDropDown.classList.remove('show');
+	 }
+      }
+   }
 }

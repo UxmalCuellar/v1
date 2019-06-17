5e5b068ab9c8e24f3344cba8e7965de537c32978
Uxmal Cuellar <uxmal.c@gmail.com>
2019-06-17 14:16:36 +1000

    Home button and scroll to top feature added to home button

--- static/script.js
+++ static/script.js
@@ -1,24 +1,30 @@
-// toggles menu icon into an x and show/hides menu items
+/* 
+ * Toggles menu icon into an x and show/hides menu items 
+ */
 function toggleMenuIcon(x) {
    x.classList.toggle("change");
    document.getElementById("menu").classList.toggle("show");
 }
 
+/*
 // close dropdown menu if user clicks outside if it
-// window.onclick = function(event) {
-//    if (!event.target.matches('.toc-holder')) {
-//       var dropdowns = document.getElementByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-// 	 var openDropDown = dropdowns[i];
-// 	 if (openDropDown.classList.contains('show')) {
-// 	    openDropDown.classList.remove('show');
-// 	 }
-//       }
-//    }
-// }
+window.onclick = function(event) {
+   if (!event.target.matches('.toc-holder')) {
+      var dropdowns = document.getElementByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+	 var openDropDown = dropdowns[i];
+	 if (openDropDown.classList.contains('show')) {
+	    openDropDown.classList.remove('show');
+	 }
+      }
+   }
+}
+*/
 
-
+/* 
+ * Show icons if at the top of the page or bottom else fade out
+ */
 $(window).scroll(function() {
    if ($(this).scrollTop()>0) {
       $('.icons').fadeOut();
@@ -28,4 +34,22 @@ $(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
       $('.icons').fadeIn();
    }
+});
+
+/*
+ * Scroll to top of page
+ */
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var top =  $(".goto-top");
+        if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
+  top.animate({"margin-left": "0px"},1500);
+        } else {
+            top.animate({"margin-left": "-100%"},1500);
+        }
+    });
+
+    $(".goto-top").on('click', function () {
+        $("html, body").animate({scrollTop: 0}, 400);
+    });
 });

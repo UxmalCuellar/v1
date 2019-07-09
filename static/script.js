3f233ef5c8c23aa89be6243bc1b66504d6dd0096
Uxmal Cuellar <uxmal.c@gmail.com>
2019-07-09 19:11:41 +1000

    added smooth scroll to links + edu logos re-aligned

--- static/script.js
+++ static/script.js
@@ -53,3 +53,21 @@ $(document).ready(function () {
         $("html, body").animate({scrollTop: 0}, 400);
     });
 });
+
+/*
+ * smooth scroll to all links
+ */
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
